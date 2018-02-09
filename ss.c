0c778bf33d985b4cc0f782d3955f19134ddaa948
ramesh114123 <36262867+ramesh114123@users.noreply.github.com>
2018-02-09 11:00:05 +0000

    Create ss.c

---
+++ ss.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conoi.h>
+int main()
+{
+int a=2;b=3;c=4;
+printf("greatest of three numbers");
+if(a>b)&&(a>c)
+{
+printf("a is greater")
+}
+elseif(b>c)
+{
+printf("b is greater ");
+}
+else
+{
+printf("c is greater");
+}
+getch();
+}
