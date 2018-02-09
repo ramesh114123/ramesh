72fb50d8e0fee0c2eb0c1c8ae6dfd0212820897b
ramesh114123 <36262867+ramesh114123@users.noreply.github.com>
2018-02-09 10:55:41 +0000

    Create sdf.c

---
+++ sdf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+{
+char a1;
+printf("enter the alphabets");
+scanf("%d",&a1);
+if(a1=='a'||a1=='b'||a1=='c'||a1=='z')
+{
+printf("alphsbets");
+}
+else
+{
+printf("not alphabet");
+}
+}
