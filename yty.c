5daaef843da4066f9f3cc252b0b733d374316335
ramesh114123 <36262867+ramesh114123@users.noreply.github.com>
2018-03-01 16:07:54 +0530

    Create yty.c

---
+++ yty.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,num;
+    printf("enter the a,b value");
+    scanf("%d%d",&a,&b);
+    {
+        for(i=a;i<=b,i++)
+        {
+            if(i%2==1)
+            printf("print the i value%d",i);
+        }
+    }
+
+}
+
