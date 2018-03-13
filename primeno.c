ae13492563e91fd0ac08719a60490eaf80530c66
ramesh114123 <36262867+ramesh114123@users.noreply.github.com>
2018-03-13 16:38:46 +0530

    Create primeno.c

---
+++ primeno.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+	int i,j,count,m,n;
+	scanf("%d%d",&m,&n);
+	for(i=m;i<n;i++)
+	{
+		count=0;
+		for(j=1;j<100;j++)
+		{
+			
+			if(i%j==0)
+			count++;
+		}
+		if(count==2)
+		printf("%d is prime\n",i);
+	}
+
+}
