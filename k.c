b2ef293353e41a5cef6d03ebc31b9422497a9d2e
ramesh114123 <36262867+ramesh114123@users.noreply.github.com>
2018-03-20 16:11:08 +0530

    Create k.c

---
+++ k.c
@@ -0,0 +1,13 @@
+#inlcue<stdio.h>
+int main()d
+{
+int n,d,m,t,i,sum=0;
+printf(i=1;i<m;i++)
+{"enter the first number and \ndifference and \nnumber of terms");
+scanf("%d%d%d",&n,&d,&m);
+for(
+t=n+(m-1)*d;
+sum=sum+t;
+}
+printf("%d",sum);
+{
